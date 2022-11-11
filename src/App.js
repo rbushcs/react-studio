063fb7d1f9c72e99b23f9e6f38a870a8b05623b2
Robert Bush <robby@roberts-mbp.devices.brown.edu>
2022-11-11 16:00:08 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+// import BakeryOption from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -8,21 +9,74 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+function BakeryOption(props) {
+  return (
+      <div>
+      <img src = {props.image}></img>
+      <h2>{props.name}</h2>
+      <h4>{props.description}</h4>
+      <h4>{props.price}</h4>
+      {/* <button onClick={() => setCount(count + {props,price})}>Add to Cart</button> */}
+      {/* <button onClick={() => setCount(props.varr + props.price)}>Add to Cart</button> */}
+      <button onClick={() => props.cart(props.prices, props.names)}>Add to Cart</button>
+      </div>
+  );
+}
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [count, setCount] = useState(0);
+  const [lister, setLister] = useState([]);
+
+  function CartMath(price, name) {
+    return (
+      <div>
+
+
+
+      {setCount(count + price)}
+      <p>{count}</p>
+      {console.log({price})}
+      {console.log({count})}
+      {console.log({name})}
+      <p>{name}</p>
+      {/* {var holder = list.concat([name])} */}
+      {setLister(lister.concat(name))}
+      {console.log({lister})}
+     
+      {/* {setList(holder)} */}
+      </div>
+      
+      
+    )
+  }
+
+  // function CartAdd(name){
+  //   return(
+  //     <div>
+
+  //     </div>
+  //   )
+  // }
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>The Bakeshops!</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        // <p>Bakery Item {index}</p> // replace with BakeryItem component
+
+        <BakeryOption key={index} name={item.name} names={item.name} image={item.image} description={item.desciption} price={item.price} prices={item.price} cart={CartMath}/>
       ))}
 
-      <div>
+      <div className="top">
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <p>Cost: ${count}</p>
+        {/* <li>{lister}</li> */}
+        {lister.map((x) => (
+          <li>Item: {x}</li>
+        ))}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,11 @@
 // TODO: create a component that displays a single bakery item
+function BakeryOption(props) {
+    return (
+        <div>
+        <img src = {props.image}></img>
+        <h2>{props.name}</h2>
+        <h4>{props.description}</h4>
+        <h4>{props.price}</h4>
+        </div>
+    );
+}
